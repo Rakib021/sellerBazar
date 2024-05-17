20c56498ce7112309c80759a2110e13b5b9e29a9
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-05-18 00:30:12 +0600

    customize authentication system built in django

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 urlpatterns = [
-    path('', views.home),
-    path('product-detail/', views.product_detail, name='product-detail'),
+    path('',views.ProductView.as_view(),name="home"),
+    path('product-detail/<int:pk>',views.ProductDetailView.as_view(), name='product-detail'),
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
@@ -10,7 +14,8 @@ urlpatterns = [
     path('orders/', views.orders, name='orders'),
     path('changepassword/', views.change_password, name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
-    path('login/', views.login, name='login'),
-    path('registration/', views.customerregistration, name='customerregistration'),
+    path('mobile/<slug:data>', views.mobile, name='mobiledata'),
+    path('accounts/login/',auth_views.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm),name='login'),
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
-]
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Customer,Product,Cart,OrderPlaced,Banner
+from .forms import RegistrationForm
+from django.contrib import messages
 
-def home(request):
- return render(request, 'app/home.html')
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+class ProductView(View):
+  def get(self, request):
+    topWears = Product.objects.filter(category ='TW')
+    bottomWears = Product.objects.filter(category ='BW')
+    mobile = Product.objects.filter(category ='M')
+    banner_img = Banner.objects.all()
+    return render(request, 'app/home.html',{
+      'topWears':topWears,
+      'bottomWears':bottomWears,
+      'mobile':mobile,
+      'banner':banner_img
+    })
+
+    
+# def product_detail(request):
+#  return render(request, 'app/productdetail.html')
+
+class ProductDetailView(View):
+  def get(self,request,pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'app/productdetail.html',{'product':product})
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
@@ -24,14 +45,33 @@ def orders(request):
 def change_password(request):
  return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
+def mobile(request,data=None):
+  if data ==None:
+    mobiles = Product.objects.filter(category='M')
+  elif data=='redmi' or data=='iphone':
+    mobiles = Product.objects.filter(category='M').filter(brand=data)
+  elif data=='below':
+    mobiles = Product.objects.filter(category='M').filter(discounted_price__lt =10000)
+  elif data=='above':
+    mobiles = Product.objects.filter(category='M').filter(discounted_price__gt =10000)
+  return render(request, 'app/mobile.html',{'mobiles':mobiles})
 
 def login(request):
  return render(request, 'app/login.html')
 
-def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+# def customerregistration(request):
+#  return render(request, 'app/customerregistration.html')
+
+class CustomerRegistrationView(View):
+  def get(self, request):
+    form = RegistrationForm()
+    return render(request, 'app/customerregistration.html',{'form':form})
+  def post(self, request):
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      messages.success(request,'Congratulation !! Registered Successfully')
+      form.save()
+    return render(request, 'app/customerregistration.html',{'form':form})
 
 def checkout(request):
  return render(request, 'app/checkout.html')
